489d41ff7f8a409810ac2c2adf516f621297932a
a_kayerov <a_kayerov@zdrav.yar.ru>
2017-04-28 12:05:44 +0300

    vers 0.0.1

--- routes/patients.js
+++ routes/patients.js
@@ -9,4 +9,32 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/', function(req, res, next) {
+      //console.log(req);
+      console.log(req.body.fname, req.body.lname);
+      Patient.create( req.body).then(function(task) {
+//         console.log('Success');
+         res.send('OK');
+      });
+});
+
+router.delete('/', function(req, res, next) {
+//      console.log(req.body.id);
+      Patient.findById(req.body.id).then(function(patient) {
+        patient.destroy();
+        res.send('OK');
+      })
+});
+
+
+router.put('/', function(req, res, next) {
+//      console.log(req.body.id);
+      Patient.findById(req.body.id).then(function(patient) {
+        patient.update(req.body).then(function() {
+          res.send('OK');
+        })
+     })
+});
+
+
 module.exports = router;
