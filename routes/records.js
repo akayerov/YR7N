cf1e5a454a05e39239a61e94ac452b1b741a17ce
a_kayerov <a_kayerov@zdrav.yar.ru>
2017-04-28 12:45:13 +0300

    record CRUD

--- routes/records.js
+++ routes/records.js
@@ -1,12 +1,42 @@
 var express = require('express');
 var router = express.Router();
-var Patient  = require('../models/Patient');
+var Record  = require('../models/Record');
 
 /* GET users listing. */
+/* GET patients listing. */
 router.get('/', function(req, res, next) {
-  var pl = Patient.findAll();
-  console.log(pl);
-  res.send(Patient.findAll());
+  Record.findAll().then(function(patients) {
+      res.send(patients);
+  });
 });
+
+
+router.post('/', function(req, res, next) {
+      //console.log(req);
+      console.log(req.body.fname, req.body.lname);
+      Record.create( req.body).then(function(task) {
+//         console.log('Success');
+         res.send('OK');
+      });
+});
+
+router.delete('/', function(req, res, next) {
+//      console.log(req.body.id);
+      Record.findById(req.body.id).then(function(Record) {
+        Record.destroy();
+        res.send('OK');
+      })
+});
+
+
+router.put('/', function(req, res, next) {
+//      console.log(req.body.id);
+      Record.findById(req.body.id).then(function(Record) {
+        Record.update(req.body).then(function() {
+          res.send('OK');
+        })
+     })
+});
+
 
 module.exports = router;
